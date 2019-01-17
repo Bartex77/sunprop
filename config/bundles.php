6714667234fea7b0ed566d229deecf48823fb8dc
Bartek Bartkowski <bartlomiej.bartkowski@gmail.com>
2019-01-17 18:06:54 +0100

    Frontend libs

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
